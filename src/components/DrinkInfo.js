d84f8a5aafa5608bce53aded6e99495d7105dd04
Jose Aguilar <jaguilarny@gmail.com>
2022-08-01 00:31:51 -0400

    Display ingredients and measurements in DrinkInfo.js

--- src/components/DrinkInfo.js
+++ src/components/DrinkInfo.js
@@ -4,6 +4,34 @@ import { useLocation } from "react-router-dom";
 export default function DrinkInfo() {
     const location = useLocation();
     const drinkInfo = location.state;
+    const ingredients = [];
+    const measurements = [];
 
-    return <h1>Drink Info for {drinkInfo.strDrink}</h1>
+    for (let [key, value] of Object.entries(drinkInfo)) {
+        if (key.startsWith('strIngredient') && value !== null) {
+            ingredients.push(value)
+        }
+        if (key.startsWith('strMeasure') && value !== null) {
+            measurements.push(value)
+        }
+    };
+    
+    //How is this working?! this is properly creating <li> elements, but how?? retuns nested array with undefined elements
+    const mixingInstructions = measurements.map((measurement, index1) => 
+                                ingredients.map((ingredient, index2) => {
+                                    if (index1 === index2) {
+                                        return <li key={ingredient}>{measurement} of {ingredient}</li>
+                                    }
+                                })
+                            )
+
+    return (
+        <div>
+            <h1>{drinkInfo.strDrink}</h1>
+            <img src={drinkInfo.strDrinkThumb} alt="drink image" />
+            <ul>
+                {mixingInstructions} 
+            </ul>
+        </div>
+    )
 }
--- src/components/DrinksContainer.js
+++ src/components/DrinksContainer.js
@@ -1,7 +1,5 @@
 import React from "react";
-import { useLocation } from "react-router-dom";
 import DrinkCard from "./DrinkCard";
-import DrinkInfo from "./DrinkInfo";
 
 export default function DrinksContainer({ drinks }) {
     const drinksDisplay = drinks.map((drink) => <DrinkCard key={drink.idDrink} drinkInfo={drink} />);
