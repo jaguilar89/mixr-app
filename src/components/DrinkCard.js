904de441d0054f32b5535080d586242709aec5dc
Jose Aguilar <jaguilarny@gmail.com>
2022-08-01 00:30:53 -0400

    Add alt attrributes to img elements

--- src/components/DrinkCard.js
+++ src/components/DrinkCard.js
@@ -6,7 +6,7 @@ export default function DrinkCard({ drinkInfo }) {
     return (
         <div className="drink-card">
             <div className="drink-thumb">
-                <img src={drinkInfo.strDrinkThumb} />
+                <img src={drinkInfo.strDrinkThumb} alt="drink thumbnail" />
             </div>
             <p className="drink-card-name">{drinkInfo.strDrink}</p>
             <Link to={{
