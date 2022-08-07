672d59996b5bb307e5e82b93e01c37b9c9b80436
Jose Aguilar <jaguilarny@gmail.com>
2022-08-07 19:28:47 -0400

    Remove Contact.js, add AddDrink.js

---
+++ src/components/AddDrink.js
@@ -0,0 +1,11 @@
+import React from "react";
+import AddDrinkForm from "./AddDrinkForm";
+
+export default function AddDrink() {
+    return (
+        <>
+        <h1>Add a Drink</h1>
+        <AddDrinkForm />
+        </>
+        )
+}
