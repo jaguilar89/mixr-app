251b0323fb16316d3175f915e9b44014911e1a08
jaguilar89 <jaguilarny@gmail.com>
2023-12-30 16:37:09 -0500

    add AbortController to account for Heroku cold start delays

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -6,20 +6,28 @@ export default function useFetch(endpoint) {
     const [error, setError] = useState(null)
 
     useEffect(() => {
+        //added AbortController to account for Heroku cold start delays
+        const controller = new AbortController()
+        const signal = controller.signal
+
         async function fetchData() {
             try {
                 setLoading(true)
-                const response = await fetch(endpoint)
+                const response = await fetch(endpoint, { signal })
                 const result = await response.json()
                 setData(result)
             } catch (error) {
-                setError(error)
+                if (error.name !== 'AbortError') {
+                    setError(error.message)
+                }
             } finally {
                 setLoading(false)
             }
         }
 
         fetchData()
+
+        return () => controller.abort()
     }, [endpoint]);
 
     return { data, loading, error }
